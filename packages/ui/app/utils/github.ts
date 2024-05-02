94e7a604f450c8dc404057c025ef63e170a5293d
Nhan Lam <lamnhan92@gmail.com>
2024-05-02 12:20:33 +0700

    Remove ui app & update prebuilt config

--- packages/ui/app/utils/github.ts
+++
@@ -1,4 +0,0 @@
-export function buildGithubRawUrl(repoUrl: string) {
-  const ownerAndRepo = repoUrl.replace(/https?:\/\/github.com\//, '');
-  return `https://raw.githubusercontent.com/${ownerAndRepo}`;
-}
